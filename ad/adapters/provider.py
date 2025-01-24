17fdbf8255a1c66c6f03972a3eb65ab1ab236c1d
Toshick <zingfrid@gmail.com>
2025-01-25 01:16:22 +0200

    Update to the latest olx version at 25.01.2025

--- ad/adapters/provider.py
+++ ad/adapters/provider.py
@@ -34,7 +34,7 @@ class _CreateProviderOlx1(CreateAdsProvider):
     @staticmethod
     def _process_item(item):
         title, *_ = item.xpath(
-            './/h4[contains(@class, "css-1s3qyje")]/text()'
+            './/h4[contains(@class, "css-1sq4ur2")]/text()'
             # css-1s3qyje
             # css-1wxaaza
             # css-16v5mdi er34gjf0
@@ -50,7 +50,7 @@ class _CreateProviderOlx1(CreateAdsProvider):
         #print (item.xpath('.//p[contains(@data-testid, "ad-price")]/text()'))
         dirty_price = item.xpath('.//p[contains(@data-testid, "ad-price")]/text()')[0]
         date = item.xpath('.//p[contains(@data-testid, "location-date")]/text()')[0]
-        name = item.xpath('.//h4[contains(@class, "css-1s3qyje")]/text()')[0]
+        name = item.xpath('.//h4[contains(@class, "css-1sq4ur2")]/text()')[0]
 #        print (item)
 #        print(title)
 #        print(name)
