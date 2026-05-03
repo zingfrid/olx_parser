b97848e64a8fd52226a66c55dece947c86e055f7
Toshick <zingfrid@gmail.com>
2026-05-03 15:44:28 +0200

    Some of the missed fixes!

--- olx/__init__.py
+++ olx/__init__.py
@@ -57,9 +57,9 @@ def fetch_ads(session: Session) -> Set[AdModel]:
 
 #        print(item)
         try:
-            price = int(
+            price = round(float(
                 item[1].split(' грн.')[0].strip().replace(' ', '')
-            )
+            ))
         except ValueError:
             logger.exception('=== Error during parsing a price ===')
             continue
